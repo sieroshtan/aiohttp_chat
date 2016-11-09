4b7e74593830d2d397a75e516aa1705244a16526
Mike <ulianin_m@mail.ru>
2016-11-09 13:28:01 +0200

    fix events of message sending

--- app/handlers.py
+++ app/handlers.py
@@ -2,7 +2,9 @@ import json
 import time
 import aiohttp
 from aiohttp import web
-from stores import messages_table, message_recipient_table
+from sqlalchemy import select
+from stores import messages_table, user_channel_table, user_table
+# from stores import messages_table, message_recipient_table
 
 
 async def websocket_handler(request):
@@ -24,28 +26,67 @@ async def websocket_handler(request):
         async for msg in ws:
             if msg.type == aiohttp.WSMsgType.TEXT:
                 event_data = json.loads(msg.data)
+
                 if event_data['type'] == 'message.im':
                     ts = time.time()
+                    channel_id = event_data['channel']
                     to_id = event_data['to']
 
                     async with db.acquire() as conn:
                         res = await conn.execute(
                             messages_table.insert().values(ts=ts,
                                                            text=event_data['text'],
-                                                           user_id=current_user_id,
+                                                           user_sender_id=current_user_id,
+                                                           channel_id=channel_id,
                                                            delivered=False,
                                                            read=False))
-                        msg_id = list(res)[0][0]
-                        await conn.execute(
-                            message_recipient_table.insert().values(message_id=msg_id, user_id=to_id))
+
+                        current_user_name = await conn.execute(
+                            select([user_table.c.username]).where(user_table.c.id == current_user_id)
+                        )
+                        # msg_id = list(res)[0][0]
+                        # await conn.execute(
+                        #     message_recipient_table.insert().values(message_id=msg_id, user_id=to_id))
 
                     event_data['ts'] = ts
                     event_data['user_id'] = current_user_id
+                    event_data['user_name'] = list(current_user_name)[0][0]
                     del event_data['to']
 
                     await nc.publish('channel.%s' % to_id, json.dumps(event_data).encode('utf-8'))
                     await nc.publish('channel.%s' % current_user_id, json.dumps(event_data).encode('utf-8'))
                     # await nc.publish('channel.%s' % current_user_id, msg.data.encode('utf-8'))
+
+                elif event_data['type'] == 'message.group':
+                    ts = time.time()
+                    channel_id = event_data['channel']
+
+                    async with db.acquire() as conn:
+                        res = await conn.execute(
+                            messages_table.insert().values(ts=ts,
+                                                           text=event_data['text'],
+                                                           user_sender_id=current_user_id,
+                                                           channel_id=event_data['channel'],
+                                                           delivered=False,
+                                                           read=False))
+
+                        channel_users = await conn.execute(
+                            select([user_channel_table.c.user_id]).where(user_channel_table.c.channel_id == channel_id)
+                        )
+                        current_user_name = await conn.execute(
+                            select([user_table.c.username]).where(user_table.c.id == current_user_id)
+                        )
+
+                    event_data['ts'] = ts
+                    event_data['user_id'] = current_user_id
+                    event_data['user_name'] = list(current_user_name)[0][0]
+
+                    # for user in list(channel_users):
+                    #     id_user = user[0]
+                    #     await nc.publish('channel.%s' % id_user, json.dumps(event_data).encode('utf-8'))
+                    for row in channel_users:
+                        to_id = row[user_channel_table.c.user_id]
+                        await nc.publish('channel.%s' % to_id, json.dumps(event_data).encode('utf-8'))
 
             elif msg.type == aiohttp.WSMsgType.ERROR:
                 print('ws connection closed with exception %s' % ws.exception())
--- app/stores.py
+++ app/stores.py
@@ -6,14 +6,36 @@ messages_table = sa.Table('chat_message', metadata,
     sa.Column('id', sa.Integer, primary_key=True),
     sa.Column('ts', sa.Unicode(45)),
     sa.Column('text', sa.Unicode(1000)),
-    sa.Column('user_id', sa.INTEGER),
+    sa.Column('user_sender_id', sa.INTEGER),
+    sa.Column('channel_id', sa.INTEGER),
     sa.Column('delivered', sa.BOOLEAN, default=False),
     sa.Column('read', sa.BOOLEAN, default=False),
 )
 
 
-message_recipient_table = sa.Table('chat_messagerecipient', metadata,
+user_channel_table = sa.Table('chat_userchannel', metadata,
     sa.Column('id', sa.Integer, primary_key=True),
-    sa.Column('message_id', sa.INTEGER, sa.ForeignKey('chat_message.id')),
-    sa.Column('user_id', sa.INTEGER, sa.ForeignKey('auth_user.id')),
+    sa.Column('user_id', sa.INTEGER),
+    sa.Column('channel_id', sa.INTEGER),
 )
+
+
+user_table = sa.Table('auth_user', metadata,
+    sa.Column('id', sa.Integer, primary_key=True),
+    sa.Column('last_login', sa.Unicode(45)),
+    sa.Column('is_superuser', sa.BOOLEAN, default=False),
+    sa.Column('password', sa.Unicode(1000)),
+    sa.Column('username', sa.Unicode(150)),
+    sa.Column('first_name', sa.Unicode(30)),
+    sa.Column('last_name', sa.Unicode(30)),
+    sa.Column('email', sa.Unicode(250)),
+    sa.Column('is_staff', sa.BOOLEAN, default=False),
+    sa.Column('is_active', sa.BOOLEAN, default=True),
+    sa.Column('date_joined', sa.Unicode(45)),
+)
+
+# message_recipient_table = sa.Table('chat_messagerecipient', metadata,
+#     sa.Column('id', sa.Integer, primary_key=True),
+#     sa.Column('message_id', sa.INTEGER, sa.ForeignKey('chat_message.id')),
+#     sa.Column('user_id', sa.INTEGER, sa.ForeignKey('auth_user.id')),
+# )
